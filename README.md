bfa159e690a786fa2e8c2152b502df181a8b9283
nanspro <rahulbishnoi3636@gmail.com>
2020-07-27 04:04:31 +0530

    updated contract

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # nuSponsor
 An application where users can sponsor their favourite creators and subscribe to their content. Built on nucypher!
+
+## Creator
+- Creator will be running a nuCypher node address of which will be entered on login
+
+- Creator signs up, enters profile metadata (handle, bio, membership fees, pubkey)
+    - Frontend -> Call createSubscriptionPage in Contract
+
+- Creator creates/uploads new post
+    - Frontend -> Creator's node, will encrypt the data 
+    - Creator Node -> return ipfsHash to -> Frontend 
+    - Frontend -> Call createPost in the contract
+
+- Creator selects who to Approve as subscribers (by granting access to policy)
+- Creator grants 
+
+## Buyer
+- Buyer will be running a nuCypher node address of which will be entered on login
+
+- Buyer can explore all the creator pages and metadata
+- Buyer subscribes to a creator 
+    - Frontend -> Call createSubscription in contract + membership fees
+    - Frontend -> Contract, pays membership fees
+    - Membership Fees are kept in escrow till creator approves new subscriber
+- Buyer status
+- Buyer joins policy
+
+- Buyer visits subscription page 
+    - Frontend -> Buyer Node will get data from IPFS 
+    - Buyer Node will decrypt -> frontend
+
--- contracts/contracts/Subscription.sol
+++ contracts/contracts/Subscription.sol
@@ -4,23 +4,31 @@ contract Subscription {
     address public owner;
 
     struct Creator {
+        bytes32 metadata;
         address account;
         uint membershipFee;
         bytes32[] posts;
-        uint subscribers;
+        bytes32 policyInfo;
     }
 
     struct Buyer {
         address account;
-        address memberOf;
+        PolicyStatus subscription;
+    }
+
+    struct PolicyStatus {
+        bytes32 policyInfo;
+        uint status;
     }
 
     mapping(address => Creator) public creators;
-    mapping(address => Buyer) public buyers;
+    mapping(address => Buyer) buyers;
+    mapping(bytes32 => PolicyStatus) public policies;
 
-    event NewSubscriptionPage(address creator);
-    event NewSubscriber(address buyer, address creator);
+    event NewSubscriptionPage(address creator, bytes32 metadata, bytes32 policyInfo, uint fees);
     event NewCreatorPost(address creator, bytes32 post);
+    event ApprovedSubscription(address creator, address buyer);
+    event NewSubscriber(address buyer, address creator);
 
     constructor() public {
         owner = msg.sender;
@@ -30,26 +38,46 @@ contract Subscription {
         if (msg.sender == owner) _;
     }
 
-    function createSubscription(uint fees) public {
-        Creator memory c;
-        c.account = msg.sender;
-        c.membershipFee = fees;
-        creators[msg.sender] = c;
-        emit NewSubscriptionPage(msg.sender);
+    modifier onlyCreator() {
+        Creator memory c = creators[msg.sender];
+        if (c.account == msg.sender) _;
     }
 
-    function createPost(bytes32 data) public {
+    // Creator creates a policy on nuCypher, passes it ot then create a subscription page
+    function createSubscriptionPage(uint fees, bytes32 metadata, bytes32 policyInfo) public {
+        Creator memory c;
+        c.account = msg.sender;
+        c.metadata = metadata;
+        c.policyInfo = policyInfo;
+        c.membershipFee = fees;
+        creators[msg.sender] = c;
+        emit NewSubscriptionPage(msg.sender, metadata, policyInfo, fees);
+    }
+
+    function createPost(bytes32 data) onlyCreator public {
         creators[msg.sender].posts.push(data);
         emit NewCreatorPost(msg.sender, data);
     }
 
-    function buySubscription(address creator) public payable {
-        require((creators[creator].membershipFee - msg.value <= 0) && (creators[creator].membershipFee > 0));
-        creators[creator].subscribers = creators[creator].subscribers + 1;
+    function approveSubscription(address buyer) onlyCreator public {
+        buyers[buyer].subscription.status = 0;
+        policies[buyers[buyer].subscription.policyInfo].status = 0;
+        // release from escrow
+        msg.sender.transfer(creators[msg.sender].membershipFee);
+        emit ApprovedSubscription(msg.sender, buyer);
+    }
+
+    function createSubscription(address creator) public payable {
+        // escrow payment
+        require(msg.value >= creators[creator].membershipFee, "Insufficient funds");
+        PolicyStatus memory p;
+        p.policyInfo = creators[creator].policyInfo;
+        p.status = 1;
         Buyer memory b;
         b.account = msg.sender;
-        b.memberOf = creator;
+        b.subscription = p;
         buyers[msg.sender] = b;
+        policies[creators[creator].policyInfo] = p;
         emit NewSubscriber(msg.sender, creator);
     }
 }
